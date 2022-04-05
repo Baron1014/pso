acc7e432217877d2c3b1c92aab509deea69ad9bd
Baron <nm6101080@gs.ncku.edu.tw>
2022-04-05 14:50:16 +0800

    add function 12

--- pso.py
+++ pso.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+from copy import copy
 
 class PSO:
     def __init__(self, n, dim, lower_bound, upper_bound, max_iters = 500):
@@ -91,47 +92,75 @@ def F3(x):
     return np.sum(power_x, axis=1).reshape(-1, 1)
 
 def F11(x):
-    sum_x = np.sum(np.power(x, 2)/4000, axis=1)
+    sum_x = np.sum(np.power(x, 2), axis=1)
     dim_index = np.arange(float(x.shape[1])) +1
     prod_x = np.prod(np.divide(np.cos(x), np.sqrt(dim_index)), axis=1)
     
-    return sum_x - prod_x + 1
+    return (sum_x/4000) - prod_x + 1
+
+def F12(x, a=10, k=100, m=4):
+    y = 1 + (x+1)/4
+    # u
+    u1 = np.where(x>a, k * np.power((x - a), m), 0)
+    u2 = np.where(x<(-1*a), k * np.power((-1*(x) - a), m), 0)
+    u = u1 + u2
+
+    # f
+    first = 10*(np.sin(np.pi*y[:, :1])**2)
+    second_1 = np.sum(np.multiply(((y[:, :-1]-1)**2), (10*(np.sin(np.pi*y[:, 1:])**2) + 1)), axis=1)
+    second_2 = (y[:, -1]-1)**2
+    second = np.array(second_1+second_2).reshape(-1, 1)
+    third = np.sum(u, axis=1).reshape(-1, 1)
+
+    return np.add((np.pi/x.shape[1])*(np.add(first, second)), third)
 
 def main():
     runs = 50
     pop = 50
-    # F2
-    print("optimize F2 function")
-    dim = 30
-    best_each_gbest = np.zeros((runs, dim)) 
-    best_each_fitness = np.zeros((runs, 1)) 
-    for i in range(runs):
-        pso = PSO(pop, dim, -10, 10, 500)
-        best_sofar, best_fitness = pso.optimal(F2)
-        best_each_gbest[i] = best_sofar
-        best_each_fitness[i] = best_fitness
-    evaluation(best_each_gbest, best_each_fitness, runs)
+    # # F2
+    # print("optimize F2 function")
+    # dim = 30
+    # best_each_gbest = np.zeros((runs, dim)) 
+    # best_each_fitness = np.zeros((runs, 1)) 
+    # for i in range(runs):
+    #     pso = PSO(pop, dim, -10, 10, 500)
+    #     best_sofar, best_fitness = pso.optimal(F2)
+    #     best_each_gbest[i] = best_sofar
+    #     best_each_fitness[i] = best_fitness
+    # evaluation(best_each_gbest, best_each_fitness, runs)
 
-    #F3
-    print("optimize F3 function")
-    dim = 30
-    best_each_gbest = np.zeros((runs, dim)) 
-    best_each_fitness = np.zeros((runs, 1)) 
-    for i in range(runs):
-        pso = PSO(pop, dim, -100, 100, 500)
-        best_sofar, best_fitness = pso.optimal(F3)
-        best_each_gbest[i] = best_sofar
-        best_each_fitness[i] = best_fitness
-    evaluation(best_each_gbest, best_each_fitness, runs)
+    # #F3
+    # print("optimize F3 function")
+    # dim = 30
+    # best_each_gbest = np.zeros((runs, dim)) 
+    # best_each_fitness = np.zeros((runs, 1)) 
+    # for i in range(runs):
+    #     pso = PSO(pop, dim, -100, 100, 500)
+    #     best_sofar, best_fitness = pso.optimal(F3)
+    #     best_each_gbest[i] = best_sofar
+    #     best_each_fitness[i] = best_fitness
+    # evaluation(best_each_gbest, best_each_fitness, runs)
 
-    #F3
-    print("optimize F11 function")
+    # #F11
+    # print("optimize F11 function")
+    # dim = 30
+    # best_each_gbest = np.zeros((runs, dim)) 
+    # best_each_fitness = np.zeros((runs, 1)) 
+    # for i in range(runs):
+    #     pso = PSO(pop, dim, -600, 600, 500)
+    #     best_sofar, best_fitness = pso.optimal(F11)
+    #     best_each_gbest[i] = best_sofar
+    #     best_each_fitness[i] = best_fitness
+    # evaluation(best_each_gbest, best_each_fitness, runs)
+
+    #F12
+    print("optimize F12 function")
     dim = 30
     best_each_gbest = np.zeros((runs, dim)) 
     best_each_fitness = np.zeros((runs, 1)) 
     for i in range(runs):
-        pso = PSO(pop, dim, -600, 600, 500)
-        best_sofar, best_fitness = pso.optimal(F11)
+        pso = PSO(pop, dim, -50, 50, 500)
+        best_sofar, best_fitness = pso.optimal(F12)
         best_each_gbest[i] = best_sofar
         best_each_fitness[i] = best_fitness
     evaluation(best_each_gbest, best_each_fitness, runs)
@@ -139,4 +168,6 @@ def main():
 if __name__=="__main__":
     main()
     # te_array = np.array([[2, 3, 2],[4, 6, 1]])
+    # te_array = np.array([[0, 0, 0],[0, 0, 0]])
     # print(F11(te_array))
+    # print(F12(te_array, 10, 100, 4))
